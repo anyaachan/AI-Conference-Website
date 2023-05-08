ecc1320449d6cb95b0bac1d9ac1196e56a213cf1
anyaachan <anna.danchenko@icloud.com>
2023-05-08 23:12:48 +0200

    update javascript html registration

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-function registrationSubmit(event) {
-    event.preventDefault();
-    document.getElementById("reg-form").submit();
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    let regForm = document.getElementById("reg-form");
+  
+    regForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log()
+      let userDame = document.getElementsByClassName("reg-input-field")
+      console.log(userDame[0].value)
+      console.log(userDame[1].value)
+    });
+  });
