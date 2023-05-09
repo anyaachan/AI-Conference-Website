c5acd63caba55944b4305fb014598eaceab0ab09
anyaachan <anna.danchenko@icloud.com>
2023-05-09 19:10:40 +0200

    add map js

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
       let userDate = document.getElementsByClassName("reg-input-field")
       console.log(userDate[0].value)
       console.log(userDate[1].value)
-
+      
     });
+    
+    let loc = [14.433824581541954, 50.08703265602295]
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW55YWFjaGFuIiwiYSI6ImNsZmdvYTNpMDA1ZmQzdnJzMnhma2xybXkifQ.x3AS6_WhkUlar_9LvRunWA';
+    let map = new mapboxgl.Map({
+    container: 'map',
+    center: loc, /* in google it is like latitute, longtitute, but it expect the reverse */
+    zoom: 14,
+    style: "mapbox://styles/mapbox/light-v11"
+});
+  map.addControl(new mapboxgl.NavigationControl()); 
+  map.scrollZoom.disable(); 
+/* creating an instance in which we constructing the new map with some information given to help */
+
   });
+
+
